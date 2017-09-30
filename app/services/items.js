9d954882885f35c9fe1bdc3959b438f5714fc6dc
Peter Schafhalter <pschafhalter@berkeley.edu>
2017-09-30 14:00:16 -0700

    Pulls item from backend

--- app/services/items.js
+++ app/services/items.js
@@ -1,11 +1,18 @@
 angular.module('app.services.items', []).factory("Items", function() {
     function getRandomItem(callback) {
-      var index = Math.floor(3 * Math.random());
-      var item = items[index];
-      callback(item);
+      var xmlHttp = new XMLHttpRequest();
+      xmlHttp.onreadystatechange = function() {
+        if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+          console.log(xmlHttp.responseText);
+          var item = JSON.parse(xmlHttp.responseText);
+          callback(item);
+        }
+      };
+      var url = "http://localhost:5000/item";
+      xmlHttp.open("GET", url, true); // true for asynchronous
+      xmlHttp.send(null);
     }
     return {
       "getRandomItem": getRandomItem,
-      'qqq': 4
     };
 });
